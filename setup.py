ef2c6c894380836d52e5cdcd072eca15adf73567
Ryan Malloy <ryan@supported.systems>
2024-08-15 21:23:43 -0600

    update maintainer, rename pypi package to django-oscar-pistachio

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from oscar_impersonate import get_version
 
 
 setup(
-    name='django-oscar-impersonate',
+    name='django-oscar-pistachio',
     version=get_version(),
-    url='https://github.com/oscaro/django-oscar-impersonate',
-    author="Nicolas Dubois",
-    author_email="nicolas.dubois@oscaro.com",
-    description="Wrapper of django-impersonate for django-oscar",
+    url='https://github.com/rsp2k/django-oscar-impersonate',
+    author="Ryan Malloy",
+    author_email="ryan@supported.systems",
+    description="🥸 Wrapper of django-impersonate for django-oscar. Fork of django-oscar-impersonate, which was abandoned. Named after Dana Carvey's character in 'The Master of Disguise (2003)'. Am I not turtley enough for the turtle club? 🐢 ",
     long_description=open('README.rst').read(),
     keywords="django, oscar, impersonate",
     license='BSD License',
@@ -23,6 +23,7 @@ setup(
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
         'Framework :: Django',
+        'Framework :: Django :: 4.0',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
@@ -30,5 +31,12 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
     ]
 )
